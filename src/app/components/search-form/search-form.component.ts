c808158188fc9efbe6ad515921ed2f42b9f14aa2
Gustavo Andrade <gustavo_alex3000@hotmail.com>
2022-01-17 23:51:15 -0300

    feat: add hero state and two-way  bind it to html input

--- src/app/components/search-form/search-form.component.ts
+++ src/app/components/search-form/search-form.component.ts
@@ -3,13 +3,18 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-search-form',
   templateUrl: './search-form.component.html',
-  styleUrls: ['./search-form.component.css']
+  styleUrls: ['./search-form.component.css'],
 })
 export class SearchFormComponent implements OnInit {
+  hero: string = '';
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
+  ngOnInit(): void {}
+  public onSubmit() {
+    if (!this.hero) {
+      //TODO: convet native alert to component alert
+      alert('Por favor busque por um personagem.');
+      return;
+    }
   }
-
 }
